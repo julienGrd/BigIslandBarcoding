29a87c7dc1cc8e86700e963e8d37da95df9b8eb9
Glenn <33450392+glenn2223@users.noreply.github.com>
2022-10-20 17:33:12 +0100

    Android focus implementation and rotate fix
    For android implemented focus and auto focus
    Also fixed roate for 1D barcode scanning
    Thanks @Captnwalker1

--- ZXing.Net.MAUI/Platforms/Android/CameraManager.android.cs
+++ ZXing.Net.MAUI/Platforms/Android/CameraManager.android.cs
@@ -1,8 +1,11 @@
 ﻿using Android.Content;
+using Android.Graphics;
+using Android.Views;
 using AndroidX.Camera.Core;
 using AndroidX.Camera.Lifecycle;
 using AndroidX.Camera.View;
 using AndroidX.Core.Content;
+using Java.Util;
 using Java.Util.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 
@@ -17,6 +20,7 @@ internal partial class CameraManager
     CameraSelector _cameraSelector = null;
     ProcessCameraProvider _cameraProvider;
     ICamera _camera;
+    Timer? _timer;
 
     Context _context;
 
@@ -54,6 +58,7 @@ internal partial class CameraManager
             // Frame by frame analyze
             _imageAnalyzer = new ImageAnalysis.Builder()
                 .SetDefaultResolution(new Android.Util.Size(640, 480))
+                .SetOutputImageRotationEnabled(true)
                 .SetBackpressureStrategy(ImageAnalysis.StrategyKeepOnlyLatest)
                 .Build();
 
@@ -68,8 +73,62 @@ internal partial class CameraManager
             );
 
             UpdateCamera();
+            AutoFocus();
+            setupAutoFocusTimer();
+            ((View)_previewView.Parent).SetOnTouchListener(new TapFocusTouchListener(this));
 
         }), ContextCompat.GetMainExecutor(_context)); //GetMainExecutor: returns an Executor that runs on the main thread.
+    }
+
+    private class TapFocusTouchListener : Java.Lang.Object, View.IOnTouchListener
+    {
+
+        private CameraManager _cameraManager;
+
+        public TapFocusTouchListener(CameraManager cameraManager)
+        {
+            this._cameraManager = cameraManager;
+        }
+
+        public bool OnTouch(View? v, MotionEvent? e)
+        {
+            if (e?.Action == MotionEventActions.Down)
+            {
+                Point point = new Point((int)e.GetX(), (int)e.GetY());
+                _cameraManager.Focus(point);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    private void setupAutoFocusTimer()
+    {
+        if (_timer != null)
+        {
+            _timer.Cancel();
+            _timer.Dispose();
+            _timer = null;
+        }
+        _timer = new Timer();
+        var task = new AFTimerTask(this);
+        _timer.ScheduleAtFixedRate(task, 5000, 5000);
+    }
+
+    private class AFTimerTask : TimerTask
+    {
+        private CameraManager _cameraManager;
+
+        public AFTimerTask(CameraManager manager)
+        {
+            this._cameraManager = manager;
+        }
+
+        public override void Run()
+        {
+            _cameraManager.AutoFocus();
+        }
     }
 
     public void Disconnect()
@@ -108,14 +167,36 @@ internal partial class CameraManager
         _camera?.CameraControl?.EnableTorch(on);
     }
 
-    public void Focus(Android.Graphics.Point point)
+    public void Focus(Point point)
     {
+        if (_camera != null && _previewView.LayoutParameters != null)
+        {
+            _camera.CameraControl?.CancelFocusAndMetering();
 
+            var factory =
+                    new SurfaceOrientedMeteringPointFactory(
+                        _previewView.LayoutParameters.Width,
+                        _previewView.LayoutParameters.Height
+                    );
+            var fpoint = factory.CreatePoint(point.X, point.Y);
+            var action = new FocusMeteringAction.Builder(fpoint, FocusMeteringAction.FlagAf)
+                                    .DisableAutoCancel()
+                                    .Build();
+
+            _camera.CameraControl?.StartFocusAndMetering(action);
+        }
     }
 
     public void AutoFocus()
     {
+        _camera?.CameraControl?.CancelFocusAndMetering();
+        var factory = new SurfaceOrientedMeteringPointFactory(1f, 1f);
+        var fpoint = factory.CreatePoint(.5f, .5f);
+        var action = new FocusMeteringAction.Builder(fpoint, FocusMeteringAction.FlagAf)
+                                //.DisableAutoCancel()
+                                .Build();
 
+        _camera?.CameraControl?.StartFocusAndMetering(action);
     }
 
     public void Dispose()
