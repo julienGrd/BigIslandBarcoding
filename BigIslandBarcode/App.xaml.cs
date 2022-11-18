697b8d89eeb6a17455e4167df3d734c557cc5fbf
Glenn <33450392+glenn2223@users.noreply.github.com>
2022-11-18 15:59:56 +0000

    Sample - better window size on "Windows" 😂

--- BigIslandBarcode/App.xaml.cs
+++ BigIslandBarcode/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
 using Application = Microsoft.Maui.Controls.Application;
 
 namespace BigIslandBarcode;
@@ -10,5 +11,18 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new NavigationPage(new MainPage());
-	}
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS // TODO: same for MACCATALYST
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new(450, (int)mauiWindow.Height));
+#endif
+        });
+    }
 }
